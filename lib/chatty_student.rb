291a03f6ead4d5179e98741f09a14d558f9b582c
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-06 21:44:35 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -10,18 +10,18 @@ def hello
 end
 end
 
-def raise_hand
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-end
+# def raise_hand
+#   super
+#   super
+#   super
+#   super
+#   super
+#   super
+#   super
+#   super
+#   super
+#   super
+# end
 
 
 end
